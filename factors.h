af7b764a283e14fea32c2f8047764b156ff6c388
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-23 12:28:08 +0000

    Header_File

---
+++ factors.h
@@ -0,0 +1,12 @@
+#ifndef FARTORS_H
+#define FARTORS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+void factorize(char *buffer);
+int factorizes(char *buffer);
+
+#endif /* FARTORS_H */
